beb6d6790b54df1804b3676c53902c9e58701c26
Prem <premkhodke.99@gmail.com>
2024-08-23 18:33:37 +0530

    Navbar & mobile and desktop view style

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import styles from './Navbar.module.css'
+
+const Navbar = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.topPortion}>
+        <div className={styles.left}>
+          <img src={'/assets/menu.png'} alt="menu" className={styles.logopic}/>
+          <img src={'/assets/Logo.png'} alt="logo" className={styles.logopic} />
+        </div>
+        <h1 className={styles.logo}>LOGO</h1>
+        <div className={styles.profile}>
+          <img src={'/assets/search-normal.png'} alt="search" className={styles.profilelogo} />
+          <img src={'/assets/heart.png'} alt="heart" className={styles.profilelogo} />
+          <img src={'/assets/shopping-bag.png'} alt="bag" className={styles.profilelogo} />
+          <img src={'/assets/profile.png'} alt="profile" className={styles.profilelogo} />
+          <select name="language" id="language">
+            <option value="eng">ENG</option>
+          </select>
+        </div>
+      </div>
+      <div className={styles.navlinks}>
+        {
+          ['shop', 'skills', 'stories', 'about', 'contact us'].map((nav, i) => (
+            <h3 key={i}>{nav}</h3>
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
